d2f708c14774b276edafe5e882c7fe018e25af59
valleykid <valleykiddy@gmail.com>
2022-04-28 09:44:38 +0800

    fix: use fullPath for baidu track

--- vuepress-docs/.vuepress/enhanceApp.js
+++ vuepress-docs/.vuepress/enhanceApp.js
@@ -3,7 +3,7 @@ export default ({ router }) => {
   router.beforeEach((to, _, next) => {
     if (typeof _hmt !== 'undefined' && !(window.ignoreDomains || []).some(domain => location.hostname.includes(domain))) {
       if (to.path) {
-        const realPath = to.fullPath.split('#')[0];
+        const realPath = to.fullPath/* .split('#')[0] */;
         _hmt.push(['_trackPageView', realPath]);
         console.log('上报百度统计', realPath);
       }
