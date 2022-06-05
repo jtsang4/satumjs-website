5392ad24e98276258cff75c0f392eff0a6e89bf1
valleykid <valleykiddy@gmail.com>
2022-06-06 07:17:32 +0800

    fix: show hmt log when local running

--- vuepress-docs/.vuepress/enhanceApp.js
+++ vuepress-docs/.vuepress/enhanceApp.js
@@ -1,8 +1,11 @@
 export default ({ router }) => {
   // 路由切换
   router.beforeEach((to, _, next) => {
-    if (typeof _hmt !== 'undefined' && !(window.ignoreDomains || []).some(domain => location.hostname.includes(domain))) {
-      if (to.path) {
+    const ignoreDomain = (window.ignoreDomains || []).some(domain => location.hostname.includes(domain));
+    if (typeof _hmt !== 'undefined' && to.path) {
+      if (ignoreDomain) {
+        console.log('[百度统计]', to.fullPath);
+      } else {
         const realPath = to.fullPath/* .split('#')[0] */;
         _hmt.push(['_trackPageView', realPath]);
         console.log('上报百度统计', realPath);
